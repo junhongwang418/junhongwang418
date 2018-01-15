27cb242820ca304d015a92618d8ad3b636174a6b
Junhong Wang <ionejunhong@gmail.com>
2018-01-14 16:56:19 -0800

    testing

--- movie-reccomendation/gallery.js
+++ movie-reccomendation/gallery.js
@@ -1,23 +1,27 @@
 var galleryDiv = document.getElementById("gallery");
 
 $.getJSON("moviedictionary.json", function(data) {
-    console.log(data["2"]);
+    console.log(data);
     // data is a JavaScript object now. Handle it as such
-
 });
 
 for (var i = 0; i < 10; i++) {
-	var div = document.createElement("div");
-	var a = document.createElement("a");
-	var img = document.createElement("img");
-	div.className += "col-lg-3 col-md-4 col-xs-6";
-	a.className += "d-block mb-4 h-100";
-	img.className += "img-fluid img-thumbnail";
-	img.src = "http://placehold.it/400x300";
-	img.alt = ""
 
-	a.appendChild(img);
-	div.appendChild(a);
+
+
+	// var div = document.createElement("div");
+	// var a = document.createElement("a");
+	// var img = document.createElement("img");
+	// div.className += "col-lg-3 col-md-4 col-xs-6";
+	// a.className += "d-block mb-4 h-100";
+	// img.className += "img-fluid img-thumbnail";
+	// img.src = "http://placehold.it/400x300";
+	// img.alt = ""
+
+	// a.appendChild(img);
+	// div.appendChild(a);
+	var p = document.createElement("p");
+	p.
 	galleryDiv.appendChild(div);
 
 }
