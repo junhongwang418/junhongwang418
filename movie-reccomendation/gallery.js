0518ff27c934dd9e52d7399682fef382e1d53f9e
Junhong Wang <ionejunhong@gmail.com>
2018-01-14 17:03:11 -0800

    testing

--- movie-reccomendation/gallery.js
+++ movie-reccomendation/gallery.js
@@ -1,33 +1,33 @@
-var galleryDiv = document.getElementById("gallery");
+
 
 $.getJSON("moviedictionary.json", function(data) {
-    console.log(data);
-    // data is a JavaScript object now. Handle it as such
+
+	var galleryDiv = document.getElementById("gallery");
+  var ul = document.createElement("ul");
+
+	for (var i = 0; i < 10; i++) {
+
+
+
+		// var div = document.createElement("div");
+		// var a = document.createElement("a");
+		// var img = document.createElement("img");
+		// div.className += "col-lg-3 col-md-4 col-xs-6";
+		// a.className += "d-block mb-4 h-100";
+		// img.className += "img-fluid img-thumbnail";
+		// img.src = "http://placehold.it/400x300";
+		// img.alt = ""
+
+		// a.appendChild(img);
+		// div.appendChild(a);
+		
+		var li = document.createElement("li");
+		var textNode = document.createElement(data["1"]);
+		li.appendChild(textNode);
+		ul.appendChild(div);
+
+	}
+
+	galleryDiv.appendChild(ul);
 });
 
-var ul = document.createElement("ul");
-
-for (var i = 0; i < 10; i++) {
-
-
-
-	// var div = document.createElement("div");
-	// var a = document.createElement("a");
-	// var img = document.createElement("img");
-	// div.className += "col-lg-3 col-md-4 col-xs-6";
-	// a.className += "d-block mb-4 h-100";
-	// img.className += "img-fluid img-thumbnail";
-	// img.src = "http://placehold.it/400x300";
-	// img.alt = ""
-
-	// a.appendChild(img);
-	// div.appendChild(a);
-	
-	var li = document.createElement("li");
-	var textNode = document.createElement(data["1"]);
-	li.appendChild(textNode);
-	ul.appendChild(div);
-
-}
-
-galleryDiv.appendChild(ul);
