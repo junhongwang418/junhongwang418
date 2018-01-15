34214a1969f6efbba1211e8aeedc4f7466401380
Junhong Wang <ionejunhong@gmail.com>
2018-01-14 17:42:35 -0800

    test

--- movie-reccomendation/gallery2.js
+++ movie-reccomendation/gallery2.js
@@ -1,45 +1,45 @@
-var moviedictionary = undefined;
-var start = 0;
-var end = 10;
-
 $.getJSON("moviedictionary.json", function(data) {
 
-	moviedictionary = data;
+	var start = 0;
+	var end = 10;
 
-});
+	movies = data.values();
 
-var movies = moviedictionary.values();
-
-$("#loadmore").click(function(){
-  var ul = document.getElementById("gallerylist");
-	
-
-	for (var i = start; i < end; i++) {
-
-		// var div = document.createElement("div");
-		// var a = document.createElement("a");
-		// var img = document.createElement("img");
-		// div.className += "col-lg-3 col-md-4 col-xs-6";
-		// a.className += "d-block mb-4 h-100";
-		// img.className += "img-fluid img-thumbnail";
-		// img.src = "http://placehold.it/400x300";
-		// img.alt = ""
-
-		// a.appendChild(img);
-		// div.appendChild(a);
+	$("#loadmore").click(function(){
+	  var ul = document.getElementById("gallerylist");
 		
-		var li = document.createElement("li");
-		var textNode = document.createTextNode(movies[i]);
-		li.appendChild(textNode);
-		ul.appendChild(li);
+		for (var i = start; i < end; i++) {
 
-	}
+			// var div = document.createElement("div");
+			// var a = document.createElement("a");
+			// var img = document.createElement("img");
+			// div.className += "col-lg-3 col-md-4 col-xs-6";
+			// a.className += "d-block mb-4 h-100";
+			// img.className += "img-fluid img-thumbnail";
+			// img.src = "http://placehold.it/400x300";
+			// img.alt = ""
 
-	start += 10;
-	end += 10;
+			// a.appendChild(img);
+			// div.appendChild(a);
+			
+			var li = document.createElement("li");
+			var textNode = document.createTextNode(movies[i]);
+			li.appendChild(textNode);
+			ul.appendChild(li);
+
+		}
+
+		start += 10;
+		end += 10;
 
 
  });
+
+
+
+
+
+});
 
 
 
