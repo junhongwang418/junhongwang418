1069d2b380ad029306dadaa328686b93c4e16ebf
Junhong Wang <ionejunhong@gmail.com>
2020-04-17 10:43:55 -0700

    enable msuci loop

--- src/music/Music.tsx
+++ src/music/Music.tsx
@@ -4,12 +4,12 @@ import { Typography, Container } from "@material-ui/core";
 const Music = () => {
 
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="lg">
       <Typography>This is music loop player. I call this JMusic.</Typography>
       <Typography variant="caption">Motivation comes from here: https://support.google.com/youtube/thread/12880369?hl=en</Typography>
       <br />
       <Typography variant="caption">https://listenonrepeat.com/ was nice but the ads were killing my CPU</Typography>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/tdhSaEbfIko?autoplay=1&playlist=tdhSaEbfIko" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/tdhSaEbfIko?autoplay=1&playlist=tdhSaEbfIko&loop=1" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </Container>
   );
 }
