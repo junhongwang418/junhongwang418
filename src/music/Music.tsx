a0a9361f8dde31b2aa03a6ed1fc8f562aeb4b97b
Junhong Wang <ionejunhong@gmail.com>
2020-04-25 14:21:23 -0700

    make adding times easier for Jmusic

--- src/music/Music.tsx
+++ src/music/Music.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Typography, Container, Grid, makeStyles, Theme, Button } from "@material-ui/core";
+import React, { useState, useEffect, useRef, } from "react";
+import { Typography, Container, Grid, makeStyles, Theme, Button, IconButton, Link } from "@material-ui/core";
 import APIManager from "../api/APIManager";
 import YoutubeVideo from "./YouTubeVideo";
 import BinarySearch from "./BinarySearch";
+import TimerIcon from '@material-ui/icons/Timer';
 
 const useStyles = makeStyles((theme: Theme) => ({
 
@@ -18,7 +19,8 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 
   logo: {
-    width: 48
+    width: 48,
+    marginRight: theme.spacing(1)
   }
 }));
 
@@ -38,18 +40,24 @@ function copyToClipboard(text) {
 const Music = () => {
 
   const classes = useStyles();
-  const [music, setMusic] = useState(APIManager.getAllMusic());
+  const [music, setMusic] = useState(null);
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
   const [currentVideoTimeIndex, setCurrentVideoTimeIndex] = useState(0);
   const [currentVideoTime, setCurrentVideoTime] = useState(0);
 
-  const [times, setTimes] = useState([]);
+  const [times, setTimes] = useState(null);
 
   const ref = useRef(null);
 
   useEffect(() => {
     setMusic(APIManager.getAllMusic());
   }, []);
+
+  useEffect(() => {
+    if (!music) return;
+
+    setTimes(music[currentVideoIndex].times);
+  }, [music, currentVideoIndex]);
  
   useEffect(() => {
 
@@ -76,13 +84,21 @@ const Music = () => {
 
   return (
     <Container maxWidth="lg">
-      <img className={classes.logo} alt="Jmusic" src="https://github.com/ioneone/Jmusic/blob/master/logo.png?raw=true" />
-      <Typography display="inline" variant="h6">
-        JMusic
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img className={classes.logo} alt="Jmusic" src="https://github.com/ioneone/Jmusic/blob/master/logo.png?raw=true" />
+        <Typography display="inline" variant="h6">
+          JMusic
+        </Typography>
+      </div>
+      <Typography variant="subtitle2">
+        About: Music Loop Player
       </Typography>
-      <Typography variant="subtitle2">Music Loop Player</Typography>
-      <Typography variant="body2">Motivation comes from here: https://support.google.com/youtube/thread/12880369?hl=en</Typography>
-      <Typography variant="body2">https://listenonrepeat.com/ was nice but the ads were killing my CPU</Typography>
+      <Typography variant="body2">
+        Motivation comes from <Link href="https://support.google.com/youtube/thread/12880369?hl=en" target="_blank">here</Link>
+      </Typography>
+      <Typography variant="body2">
+        <Link href="https://listenonrepeat.com/ " target="_blank">ListenOnRepeat</Link> was nice but the ads were killing my CPU
+      </Typography>
       <Grid container spacing={2}>
         <Grid item xs={7}>
           {
@@ -105,46 +121,53 @@ const Music = () => {
             />
           }
         </Grid>
-        <Grid item xs={5} style={{ height: 420, overflow: "auto", paddingTop: 210, paddingBottom: 210 }}>
+        <Grid item xs={5} style={{ height: 420, overflowY: "auto", paddingTop: 210, paddingBottom: 210 }}>
           {
-            music && music[currentVideoIndex].lyrics.map((line, index) => 
-              <Typography 
-                ref={index === currentVideoTimeIndex ? ref : undefined}
-                className={index === currentVideoTimeIndex ? classes.active : classes.inactive } 
-                align="center" 
-                variant="body1"
-              >
-                {line}
-              </Typography>
+            music && times && music[currentVideoIndex].lyrics.map((line, index) => 
+              <Grid container justify="center" alignItems="center">
+                <Grid item xs={9}>
+                  <Typography 
+                    ref={index === currentVideoTimeIndex ? ref : undefined}
+                    className={index === currentVideoTimeIndex ? classes.active : classes.inactive } 
+                    align="center"
+                    variant="body1"
+                  >
+                    {line}
+                  </Typography> 
+                </Grid>
+                <Grid item xs={1} style={{ textAlign: "right" }}>
+                  <IconButton 
+                    size="small"
+                    onClick={() => {
+                      setTimes([...times.slice(0, index), currentVideoTime, ...times.slice(index + 1)]);
+                    }}
+                  >
+                    <TimerIcon />
+                  </IconButton>
+                </Grid>
+                <Grid item xs={2}>
+                  <Typography 
+                    align="center"
+                    variant="body1"
+                  >
+                    {times[index] ? times[index].toFixed(2) : "..."}
+                  </Typography> 
+                </Grid>
+              </Grid>
             )
           }
         </Grid>
       </Grid>
-      {
-        currentVideoTime
-      }
+      <Typography>
+        Current Video Time: {currentVideoTime}
+      </Typography>
       <br/>
-      {
-        music && music[currentVideoIndex].times[currentVideoTimeIndex]
-      }
-      <br/>
-      <Button 
-        onClick={() => {
-          setTimes([...times, currentVideoTime]);
-        }}
-      >
-        Push
-      </Button>
-      <br />
-      {
-        times.slice(Math.max(times.length - 5, 0)).map(time => <Typography>{time}</Typography>)
-      }
       <Button
         onClick={() => {
           copyToClipboard(JSON.stringify(times));
         }}
       >
-        Copy to Clipboard
+        Copy lyrics times to Clipboard
       </Button>
     </Container>
   );
